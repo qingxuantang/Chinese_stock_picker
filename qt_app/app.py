94fff26c2421eeba5a63d2ff78d87043b461622f
Mark Zhou <xiucatwithmark@gmail.com>
2023-08-27 22:51:24 +0800

    Add all-in-one-action button in app.py

--- qt_app/app.py
+++ qt_app/app.py
@@ -5,10 +5,11 @@ def main():
     import streamlit as st 
     from langchain.llms import OpenAI
     from langchain.prompts import PromptTemplate
-    from langchain.chains import LLMChain, SequentialChain 
+    from langchain.chains import LLMChain
     from langchain.memory import ConversationBufferMemory
     from bs4 import BeautifulSoup
     import requests
+    import pandas as pd
     from importlib import reload
     from . import utils #relative import
     utils = reload(utils)
@@ -17,88 +18,91 @@ def main():
     os.environ['OPENAI_API_KEY'] = openai_apikey
     config = utils.config
 
-
+    
     # App framework
     st.title('🦜🔗 A-Shares Stock Picker')
-    query = st.text_input("请输入你想了解的股票名称或股票代码:")
-    
+    #query = st.text_input("请输入你想了解的股票名称或股票代码:")
+
 
     #Leveraging google search results via streamlit.
-    def googleSearch(query):
-        '''Return Google search results.'''
-        from googlesearch import search
-        results = []
-        for j in search(query, num=5, start=0, stop=5, pause=2.0):
-            results.append(j)
-        return results
+    #def googleSearch(query):
+    #    '''Return Google search results.'''
+    #    from googlesearch import search
+    #    results = []
+    #    try:
+    #        for j in search(query, num=5, start=0, stop=5, pause=2.0):
+    #            results.append(j)
+    #    except TypeError:
+    #        for j in search(query, num_results=5):       
+    #            results.append(j)
+    #    return results
 
 
-    def queryToPrompt():
-        results = googleSearch(query+'的股票代码')
-        prompt = []
-        for result in results:
-            try:
-                response = requests.get(result)
-                soup = BeautifulSoup(response.text, 'html.parser')
-                title = soup.title.string if soup.title else ""
-                snippet = soup.find('meta', attrs={'name': 'description'})
-                snippet = snippet['content'] if snippet else ""
-                prompt.append(f"Title: {title}\nSnippet: {snippet}")
+    #def queryToPrompt():
+    #    results = googleSearch(query+'的股票代码')
+    #    prompt = []
+    #    for result in results:
+    #        try:
+    #            response = requests.get(result)
+    #            soup = BeautifulSoup(response.text, 'html.parser')
+    #            title = soup.title.string if soup.title else ""
+    #            snippet = soup.find('meta', attrs={'name': 'description'})
+    #            snippet = snippet['content'] if snippet else ""
+    #            prompt.append(f"Title: {title}\nSnippet: {snippet}")
                 #st.write(f"Title: {title}")
                 #st.write(f"Snippet: {snippet}")
-            except requests.exceptions.SSLError:
-                pass
-        return prompt
+    #        except requests.exceptions.SSLError:
+    #            pass
+    #    return prompt
     
-    google_search_result = queryToPrompt()
+    #google_search_result = queryToPrompt()
 
     # Prompt templates
-    symbol_template = PromptTemplate(
-        input_variables = ['symbol','google_search_result'], 
-        template='''根据以下信息，告诉我 {symbol} 的股票代码是什么。 {google_search_result} 
-                    另外请根据以下提示，告诉我 {symbol} 的交易所代号。提示：如果股票代码第一位数为0或者3，则交易所代号为sz；
-                    如果股票代码第一位数为6或者9，则交易所代号为sh。
-                    最终答案请只提供股票名称，股票代码数字和交易所代号，用英文逗号将三者分隔。'''
-    )
+    #symbol_template = PromptTemplate(
+    #    input_variables = ['symbol','google_search_result'], 
+    #    template='''根据以下信息，告诉我 {symbol} 的股票代码是什么。 {google_search_result} 
+    #                另外请根据以下提示，告诉我 {symbol} 的交易所代号。提示：如果股票代码第一位数为0或者3，则交易所代号为sz；
+    #                如果股票代码第一位数为6或者9，则交易所代号为sh。
+    #                最终答案请只提供股票名称，股票代码数字和交易所代号，用英文逗号将三者分隔。'''
+    #)
     # Memory 
-    symbol_memory = ConversationBufferMemory(input_key='symbol', memory_key='chat_history')
+    #symbol_memory = ConversationBufferMemory(input_key='symbol', memory_key='chat_history')
     # LLMs
-    llm = OpenAI(temperature=0.9) 
-    symbol_chain = LLMChain(llm=llm, prompt=symbol_template, verbose=True, memory=symbol_memory) #, output_key='title'
-
-
-
-
-
-    # Show stuff to the screen if there's a user input query
-    if query:
-        st.write(google_search_result)
-        input = {'symbol': query, 'google_search_result': google_search_result}
-        title = symbol_chain.run(input)
-        st.write(title)
-        with st.expander('Symbol History'): 
-            st.info(symbol_memory.buffer)
+    #llm = OpenAI(temperature=0.9) 
+    #symbol_chain = LLMChain(llm=llm, prompt=symbol_template, verbose=True, memory=symbol_memory) #, output_key='title'
+    
 
 
     st.write('可根据需要修改研报下载参数。')
     end_page = st.number_input('下载研报数据的页数（新报告在最前，每页50条）:', value=config['end_page'])
-
-    # Button for executing the eastmoney_parser
-    if st.button('下载最新券商研报'):
-        from . import eastmoney_parser
-        eastmoney_parser = reload(eastmoney_parser)
-        scraper = eastmoney_parser.ReportScraper()
-        scraper.run(end_page=end_page)
-        st.write('Report data gathering complete.')
-
 
     st.write('可根据需要修改偿债因子参数。')
     solvency_ratio_margin = st.number_input('短期偿债因子不低于（越高越好）:', value=config['solvency_ratio_margin'])
     price_change_margin_lowerbound = st.number_input('过去一年历史收益不低于:', value=config['price_change_margin_lowerbound'])
     price_change_margin_higherbound = st.number_input('过去一年历史收益不高于:', value=config['price_change_margin_higherbound'])
     
-    # Button for executing the short-term solvency ratio calculation
-    if st.button('计算短期偿债因子'):
+    st.write('可根据需要修改凯利因子参数。')
+    kelly_fraction = st.number_input('凯利公式持仓比例（1为100%持仓）:', value=config['kelly_fraction'])
+    max_lookback_years = st.number_input('回看历史数据年数（不足一年以小数形式体现）:', value=config['max_lookback_years'])
+    capital = st.number_input('总资本:', value=config['capital'])
+    
+   
+    
+
+    #***************************************************
+    
+    # Button for all actions.
+    if st.button('开始寻股'):
+        st.write(utils.printoutHeader())
+        st.write('首先开始下载研报数据……')
+        
+        from . import eastmoney_parser
+        eastmoney_parser = reload(eastmoney_parser)
+        scraper = eastmoney_parser.ReportScraper()
+        scraper.run(end_page=end_page)
+        st.write(utils.printoutHeader())
+        st.write('研报下载完成。开始计算短期偿债因子……')
+
         from . import ratio_calculator
         ratio_calculator = reload(ratio_calculator)
         file_path = config['broker_picked_stock_path']
@@ -106,17 +110,36 @@ def main():
         calculator.calculate(solvency_ratio_margin=solvency_ratio_margin,
                              price_change_margin_lowerbound=price_change_margin_lowerbound,
                              price_change_margin_higherbound=price_change_margin_higherbound)
-        st.write('Short-term Solvency Ratio calculation complete.')
+        st.write(utils.printoutHeader())
+        st.write('短期偿债因子计算完成。开始计算凯利持仓建议……')
 
-
-    st.write('可根据需要修改凯利因子参数。')
-    kelly_fraction = st.number_input('凯利公式持仓比例（1为100%持仓）:', value=config['kelly_fraction'])
-    max_lookback_years = st.number_input('回看历史数据年数（不足一年以小数形式体现）:', value=config['max_lookback_years'])
-    capital = st.number_input('总资本:', value=config['capital'])
-    
-    if st.button('计算凯利持仓因子'):
         from . import kelly
         kelly = reload(kelly)
         kc = kelly.KellyCriterion()
         kc.calculateKC(kelly_fraction=kelly_fraction,max_lookback_years=max_lookback_years,capital=capital)
-        st.write('Kelly Criterion optimization complete.')
+        st.write(utils.printoutHeader())
+        st.write('凯利公式持仓比例计算完成。持仓结果如下：')
+
+        data_path = config['data_path']
+        kc_file_path = data_path+'kelly/grpKCResultData/tblStockPickedKC.csv'
+        df = pd.read_csv(kc_file_path)
+        st.dataframe(df)
+
+
+
+
+    
+    #***************************************************
+
+
+
+
+    
+    # Show stuff to the screen if there's a user input query
+    #if query:
+    #    st.write(google_search_result)
+    #    input = {'symbol': query, 'google_search_result': google_search_result}
+    #    title = symbol_chain.run(input)
+    #    st.write(title)
+    #    with st.expander('Symbol History'): 
+    #        st.info(symbol_memory.buffer)
