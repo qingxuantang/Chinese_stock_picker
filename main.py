174b9197aa552fcfdff244e7bfaba534202cc2b5
Mark Zhou <xiucatwithmark@gmail.com>
2023-08-27 22:49:44 +0800

    Update main.py with Threading.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
+import threading
 from importlib import reload
 from qt_app import app,utils
 app = reload(app)
@@ -8,26 +9,8 @@ config = utils.config
 
 if __name__ == '__main__' :
 
+    thread = threading.Thread(target=app.main())
+    thread.start()
     # Run app.py for streamlit app
-    app.main()
+    #app.main()
     
-    #from qt_app import eastmoney_parser,ratio_calculator,kelly
-    #eastmoney_parser = reload(eastmoney_parser)
-    #ratio_calculator = reload(ratio_calculator)
-    #kelly = reload(kelly)
-
-    #'''Run eastmoney_parser for broker reports data gathering...'''
-    #scraper = eastmoney_parser.ReportScraper()
-    #scraper.run()
-    #'''Report data gathering complete.'''
-
-    #'''Run ratio_calculator for Short-term Solvency Ratio calculation...'''
-    #file_path = config['broker_picked_stock_path']
-    #calculator = ratio_calculator.ShortTermSolvencyCalculator(config, file_path)
-    #calculator.calculate()
-    #'''Short-term Solvency Ratio calculation complete.'''
-
-    #'''Run Kelly Criterion for portfolio optimization...'''
-    #kc = kelly.KellyCriterion()
-    #kc.calculateKC()
-    #'''Kelly Criterion optimization complete.'''
