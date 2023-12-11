23741f247efbd128e9dde1431e062ad0da0c6696
Mark Zhou <xiucatwithmark@gmail.com>
2023-12-11 13:01:41 +0800

    Add email, yfinance module.

--- qt_app/utils.py
+++ qt_app/utils.py
@@ -1,4 +1,4 @@
-# -*- coding: UTF-8 -*-
+
 
 import json
 from sqlalchemy import create_engine
@@ -14,6 +14,10 @@ import time
 #from sklearn.preprocessing import MinMaxScaler
 import openai
 import pandas_datareader as pdr
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+import smtplib
+import pytz
 
 
 pkg_path = 'qt_app/'
@@ -34,7 +38,7 @@ interval_rf = ten_year_yield.iloc[-1].values[0] / 100 #Retreive the latest value
 
 def loadJSON(json_name):
     import json
-    with open('./'+pkg_path+json_name+'.json', encoding='utf-8') as f:
+    with open('./'+pkg_path+json_name+'.json', encoding='utf-8') as f: #Can not use GBK here.
         json_data = json.load(f)
     return json_data
 
@@ -53,7 +57,7 @@ def exceptionLog(pkg_path,filename,func_name,error,loop_item): #230303update
 
 def listingSuffixForParsing(exchange,symbol):
     '''Add corresponding suffix to the symbol for parsing via different APIs.'''
-    if exchange != None and symbol == None:
+    if exchange != '': #and symbol == None:
         if exchange == 'hkex': #Hong Kong Stock Exchange
             e_alphaVantage = '.HKG'
             e_yahooFinance = '.HK'
@@ -63,24 +67,21 @@ def listingSuffixForParsing(exchange,symbol):
         elif exchange == 'sz': #Shenzhen Stock Exchange
             e_alphaVantage = '.SHZ'
             e_yahooFinance = '.SZ'
-        elif exchange == 'nyse':
-            e_alphaVantage = ''
-            e_yahooFinance = ''
-        elif exchange == 'nasdaq':
+        elif exchange == 'us': #nyse,nasdaq
             e_alphaVantage = ''
             e_yahooFinance = ''
         elif exchange == '':
             e_alphaVantage = ''
             e_yahooFinance = ''
 
-    if exchange == None and symbol != None:
+    elif exchange == '': #and symbol != None:
         if symbol[0] == '0' or symbol[0] == '3':
             e_alphaVantage = '.SHZ'
             e_yahooFinance = '.SZ'
         elif symbol[0] == '6' or symbol[0] == '9':
             e_alphaVantage = '.SHH'
             e_yahooFinance = '.SS'
-        elif symbol[0] == '8' or symbol[0] == '4': # Beijing Stock Exchange
+        elif symbol[0] == '8' or symbol[0] == '4' or symbol[:3] == '920': # Beijing Stock Exchange
             e_alphaVantage = '' # Leave blank for the moment
             e_yahooFinance = ''    
 
@@ -95,9 +96,44 @@ def savingDfToCsv(path_head,exchange,path_tail,df_name,data_path,mode='w',st='',
         df_name.to_csv(s,index=index,encoding='GBK')
 
 
-def loadDfFromCsv(path_head,exchange,path_tail,data_path,st='',interval='',i_n_str='',path_add='',suffix='',folder_path='',pkg_path=pkg_path):
+def loadDfFromCsv(path_head,path_tail,data_path,exchange='',st='',interval='',i_n_str='',path_add='',suffix='',folder_path='',pkg_path=pkg_path):
     df = pd.read_csv(data_path + pkg_path + folder_path + path_head + st + exchange + i_n_str + interval + path_add + suffix + path_tail, encoding='GBK')
     return df
 
 
+#********************************************
+def symbolPickedToHtml(df):
+    html = """\
+    <html>
+      <head></head>
+      <body>
+        {0}
+      </body>
+    </html>
+    """.format(df.to_html())
+
+    df2email = MIMEText(html, 'html')
+    
+    return df2email
+
+
+
+def msgToEmail(part,market_source_key,subject_head='qt_app'):
+    with open('./'+pkg_path+'alertmail.json') as f:
+        mail = json.load(f)
+    utc_timestamp = datetime.now(pytz.utc) #utc时区的当前时间
+    utc_timestamp_all = utc_timestamp.strftime('%Y-%m-%d %H:%M')
+    msg = MIMEMultipart()
+    msg['Subject'] = subject_head + ' - ' + market_source_key + ' winners at ' + utc_timestamp_all + 'UTC'
+    msg['From'] = '874725113@qq.com'
+    address = mail['address_qq']
+    password = mail['password_qq']
+    mailto = ['874725113@qq.com','xiucatwithmark@gmail.com']
+    msg.attach(part)
+    mailServer = smtplib.SMTP('smtp.qq.com' , 587)
+    mailServer.starttls()
+    mailServer.login(address, password)
+    mailServer.sendmail(address, mailto, msg.as_string())
+    mailServer.quit()
+#********************************************
 
